599f1c849d1773520ed02b6020b5aa1191a10b3c
Nao Takeuchi <naonao2dx@gmail.com>
2018-03-09 21:05:16 +0900

    WebServer configuration file is enabled

---
+++ src/server/web/ConfigWebServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 竹内 直 on 2018/03/09.
+//
+
+#include "ConfigWebServer.h"
---
+++ src/server/web/ConfigWebServer.h
@@ -0,0 +1,14 @@
+//
+// Created by 竹内 直 on 2018/03/09.
+//
+
+#ifndef NETWORKSERVER_CONFIGWEBSERVER_H
+#define NETWORKSERVER_CONFIGWEBSERVER_H
+
+
+class ConfigWebServer {
+
+};
+
+
+#endif //NETWORKSERVER_CONFIGWEBSERVER_H
